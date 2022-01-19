43e6fab873cbca46380c3a0728cdf1459a138304
rakatubares <rakatubares@gmail.com>
2022-01-19 14:29:27 +0700

    add state sync for pdf

--- src/views/penindakan/pengaman/DisplayPengaman.vue
+++ src/views/penindakan/pengaman/DisplayPengaman.vue
@@ -88,54 +88,6 @@
 					title="Petugas 2"
 					:data.sync="data_pengaman.penindakan.petugas2"
 				/>
-				<!-- <CRow class="mb-1">
-					<CCol md="3">
-						<b>Nama Pemilik/Saksi</b>
-					</CCol>
-					<CCol md="9">
-						{{ disp_nama_saksi }}
-					</CCol>
-				</CRow>
-				<CRow class="mb-1">
-					<CCol md="3">
-						<b>Alamat</b>
-					</CCol>
-					<CCol md="9">
-						{{ disp_alamat_saksi }}
-					</CCol>
-				</CRow>
-				<CRow class="mb-1">
-					<CCol md="3">
-						<b>Pekerjaan</b>
-					</CCol>
-					<CCol md="9">
-						{{ disp_pekerjaan_saksi }}
-					</CCol>
-				</CRow>
-				<CRow class="mb-1">
-					<CCol md="3">
-						<b>Identitas</b>
-					</CCol>
-					<CCol md="9">
-						{{ disp_identitas }}
-					</CCol>
-				</CRow>
-				<CRow class="mb-1">
-					<CCol md="3">
-						<b>Petugas 1</b>
-					</CCol>
-					<CCol md="9">
-						{{ disp_petugas1 }}
-					</CCol>
-				</CRow>
-				<CRow class="mb-1">
-					<CCol md="3">
-						<b>Petugas 2</b>
-					</CCol>
-					<CCol md="9">
-						{{ disp_petugas2 }}
-					</CCol>
-				</CRow> -->
 			</CCol>
 		</CRow>
 	</div>
@@ -187,18 +139,6 @@ export default {
 		disp_alasan_pengamanan() { return this.data_pengaman.alasan_pengamanan || '-' },
 		disp_lokasi_pengamanan() { return this.data_pengaman.penindakan.lokasi_penindakan || '-' },
 		disp_keterangan() { return this.data_pengaman.keterangan || '-' },
-		// disp_nama_saksi() { return this.data_pengaman.penindakan.saksi.nama || '-' },
-		// disp_alamat_saksi() { return this.data_pengaman.penindakan.saksi.alamat || '-' },
-		// disp_pekerjaan_saksi() { return this.data_pengaman.penindakan.saksi.pekerjaan || '-' },
-		// disp_identitas() { return (this.data_pengaman.penindakan.saksi.jenis_identitas || '') + ' ' + (this.data_pengaman.penindakan.saksi.nomor_identitas || '-') },
-		// disp_petugas1() { return this.data_pengaman.penindakan.petugas1.name || '-' },
-		// disp_petugas2() { 
-		// 	if (this.data_pengaman.penindakan.petugas2 != null) {
-		// 		return this.data_pengaman.penindakan.petugas2.name
-		// 	} else {
-		// 		return '-'
-		// 	}
-		// },
 	},
 	methods: {
 		async getData() {
--- src/views/penindakan/pengaman/ModalPengaman.vue
+++ src/views/penindakan/pengaman/ModalPengaman.vue
@@ -1,7 +1,6 @@
 <template>
 	<div class="wrapper">
 		<MyModalDoc
-			ref="modal_doc"
 			title="Data BA Tanda Pengaman"
 			:state.sync="modal_state"
 			@close-modal="closeModal"
@@ -33,8 +32,8 @@
 			</template>
 			<template #tab-pdf>
 				<MyDisplayPdf 
-					ref="display_pdf"
 					v-if="['show','edit'].includes(modal_state)"
+					:state.sync="modal_state"
 					:doc_type="doc_type" 
 					:doc_id.sync="doc_id"
 				/>
@@ -50,48 +49,6 @@ import MyDisplayPengaman from './DisplayPengaman.vue'
 import MyFormDetail from '../../details/Options/FormDetail.vue'
 import MyFormPengaman from './FormPengaman.vue'
 import MyModalDoc from '../../components/ModalDoc2.vue'
-
-// const data_default = {
-// 	main: {
-// 		data: {
-// 			id: null,
-// 			no_dok_lengkap: null,
-// 			alasan_pengamanan: null,
-// 			keterangan: null,
-// 			jenis_pengaman: null,
-// 			jumlah_pengaman: null,
-// 			nomor_pengaman: null,
-// 			tempat_pengaman: null,
-// 		}
-// 	},
-// 	penindakan: {
-// 		tanggal_penindakan: null,
-// 		lokasi_penindakan: null,
-// 		sprint: {
-// 			id: null,
-// 			nomor_sprint: null,
-// 			tanggal_sprint: null
-// 		},
-// 		saksi: {
-// 			id: null,
-// 			nama: null
-// 		},
-// 		petugas1: {
-// 			user_id: null,
-// 			name: null,
-// 			nip: null
-// 		},
-// 		petugas2: {
-// 			user_id: null,
-// 			name: null,
-// 			nip: null
-// 		}
-// 	},
-// 	objek: {
-// 		type: null,
-// 		data: null
-// 	},
-// }
 
 export default {
 	name: 'ModalPengaman',
@@ -111,29 +68,10 @@ export default {
 		return {
 			doc_type: 'pengaman',
 			doc_id: this.id,
-			// data: JSON.parse(JSON.stringify(data_default)),
 			modal_state: this.state
 		}
 	},
 	methods: {
-		// async getData() {
-		// 	let data = await this.$refs.modal_doc.getData(this.doc_id)
-		// 	if (data.penindakan.petugas2 == null) {
-		// 		data.penindakan.petugas2 = {user_id: null}
-		// 	}
-		// 	if (data.objek == null) {
-		// 		data.objek = {
-		// 			type: null,
-		// 			data: null
-		// 		}
-		// 	}
-		// 	this.data = data
-		// },
-		// async refreshData(id) {
-		// 	this.doc_id = id != undefined ? id : this.doc_id
-		// 	await this.getData()
-		// 	await this.$refs.display_pdf.getPdf()
-		// },
 		closeModal() {
 			this.$emit('close-modal')
 		},
@@ -143,11 +81,6 @@ export default {
 			this.$emit('update:state', val)
 		}
 	},
-	// async mounted() {
-	// 	if (['edit', 'show'].includes(this.state)) {
-	// 		await this.getData()
-	// 	}
-	// }
 }
 </script>
 
