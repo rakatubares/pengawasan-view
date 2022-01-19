996375cbd229d3438fc406776cd35928a0f08958
rakatubares <rakatubares@gmail.com>
2022-01-19 11:28:53 +0700

    make sync state for pdf

--- src/views/penindakan/sbp/ModalSbp.vue
+++ src/views/penindakan/sbp/ModalSbp.vue
@@ -1,34 +1,10 @@
 <template>
 	<div class="wrapper">
 		<MyModalDoc
-			ref="modal_doc"
 			title="Data SBP"
 			:state.sync="modal_state"
 			@close-modal="closeModal"
 		>
-		<!-- @change-tab="checkTab" -->
-			<!-- Action buttons -->
-			<!-- <template #action-buttons>
-				<MyModalLphp
-					v-if="show_lphp_button"
-					:state="lphp_state"
-					:doc_type="doc_type"
-					:doc_id="doc_id"
-					:doc_num="data.main.data.no_dok_lengkap"
-					:data.sync="data.dokumen.lphp"
-					@submit-data="refreshData(doc_id)"
-				/>
-				<MyModalLp
-					v-if="show_lp_button"
-					:state="lp_state"
-					:doc_type="doc_type"
-					:doc_id="doc_id"
-					:doc_num="data.main.data.no_dok_lengkap"
-					:data.sync="data.dokumen.lp"
-					@submit-data="refreshData(doc_id)"
-				/>
-			</template> -->
-
 			<!-- Documents' components -->
 			<template #tab-uraian>
 				<MyDisplaySbp 
@@ -65,8 +41,8 @@
 			</template>
 			<template #tab-pdf>
 				<MyDisplayPdf 
-					ref="display_pdf"
 					v-if="['show','edit'].includes(modal_state)"
+					:state.sync="modal_state"
 					:doc_type="doc_type" 
 					:doc_id.sync="doc_id"
 				/>
@@ -83,8 +59,6 @@ import MyFormDetail from '../../details/Options/FormDetail.vue'
 import MyFormSbp from './FormSbp.vue'
 import MyFormTindakan from './FormTindakan.vue'
 import MyModalDoc from '../../components/ModalDoc2.vue'
-// import MyModalLp from './ModalLp.vue'
-// import MyModalLphp from './ModalLphp.vue'
 
 export default {
 	name: 'ModalSbp',
@@ -96,8 +70,6 @@ export default {
 		MyFormSbp,
 		MyFormTindakan,
 		MyModalDoc,
-		// MyModalLp,
-		// MyModalLphp,
 	},
 	props: {
 		state: String,
@@ -128,7 +100,9 @@ export default {
 	},
 	watch: {
 		modal_state: function(val) {
-			this.$refs.modal_doc.modal_state = val
+			if (val == 'show') {
+				this.show_tindakan = false
+			}
 			this.$emit('update:state', val)
 		},
 	},
