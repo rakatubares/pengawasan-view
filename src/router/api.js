2ee560463bb2e255cc9c1b4f070dfa6333e43096
rakatubares <rakatubares@gmail.com>
2022-02-03 09:38:12 +0700

    add place of birth and religion input

--- src/router/api.js
+++ src/router/api.js
@@ -6,10 +6,6 @@ api.getSprint = () => {return base_url + '/sprint'}
 api.getSprintById = (id) => {return base_url + '/sprint/' + id}
 api.searchSprint = () => {return base_url + '/sprint/search'}
 
-api.getEntitas = () => {return base_url + '/entitas'}
-api.getEntitasById = (id) => {return base_url + '/entitas/' + id}
-api.searchEntitas = () => {return base_url + '/entitas/search'}
-
 api.user = () => {return base_url + '/user'}
 
 api.getJabatan = () => {return base_url + '/jabatan'}
--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,6 @@ const TheContainer = () => import('@/containers/TheContainer')
 const Homepage = () => import('@/views/Homepage')
 
 // Views - Penindakan
-const PageSbp = () => import('@/views/penindakan/sbp/PageSbp')
-const PageSegel = () => import('@/views/penindakan/segel/PageSegel')
-const PageBukaSegel = () => import('@/views/penindakan/bukasegel/PageBukaSegel')
-const PageTitip = () => import('@/views/penindakan/titip/PageTitip')
-const PageTegah = () => import('@/views/penindakan/tegah/PageTegah')
 
 Vue.use(Router)
 
@@ -38,31 +33,6 @@ function configRoutes () {
 					name: 'Homepage',
 					component: Homepage
 				},
-				{
-					path: 'sbp',
-					name: 'SBP',
-					component: PageSbp
-				},
-				// {
-				// 	path: 'segel',
-				// 	name: 'Segel',
-				// 	component: PageSegel
-				// },
-				// {
-				// 	path: 'bukasegel',
-				// 	name: 'BukaSegel',
-				// 	component: PageBukaSegel
-				// },
-				// {
-				// 	path: 'titip',
-				// 	name: 'Penitipan',
-				// 	component: PageTitip
-				// },
-				// {
-				// 	path: 'tegah',
-				// 	name: 'Penegahan',
-				// 	component: PageTegah
-				// },
 			]
 		},
 	]
