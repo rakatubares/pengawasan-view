71b47c4c604d3a7c634196e8d77fb4710a8517c2
rakatubares <rakatubares@gmail.com>
2022-02-17 09:29:01 +0700

    reshape menu

--- src/containers/_mynav.js
+++ src/containers/_mynav.js
@@ -9,27 +9,37 @@ export default [
 				icon: 'chart'
 			},
 			{
-				_name: 'CSidebarNavTitle',
-				_children: ['Penindakan']
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'BA Segel',
-				to: '/segel',
-				icon: 'cil-file'
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'BA Buka Segel',
-				to: '/bukasegel',
-				icon: 'cil-file'
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'BA Penitipan',
-				to: '/titip',
-				icon: 'cil-file'
-			},
+				_name: 'CSidebarNavDropdown',
+				name: 'PENINDAKAN',
+				icon: 'cil-clone',
+				_children: [
+					{
+						_name: 'CSidebarNavDropdown',
+						name: 'Berita Acara',
+						icon: 'cil-list',
+						_children: [
+							{
+								_name: 'CSidebarNavItem',
+								name: 'BA Segel',
+								to: '/segel',
+								icon: 'cil-file'
+							},
+							{
+								_name: 'CSidebarNavItem',
+								name: 'BA Buka Segel',
+								to: '/bukasegel',
+								icon: 'cil-file'
+							},
+							{
+								_name: 'CSidebarNavItem',
+								name: 'BA Penitipan',
+								to: '/titip',
+								icon: 'cil-file'
+							},
+						]
+					}
+				]
+			}
 		]
 	}
 ]
