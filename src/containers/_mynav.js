57c99aa8a16278f6916a5dafab8ea0cb5195751f
rakatubares <rakatubares@gmail.com>
2022-02-17 08:59:44 +0700

    reshape menu

--- src/containers/_mynav.js
+++ src/containers/_mynav.js
@@ -9,38 +9,48 @@ export default [
 				icon: 'chart'
 			},
 			{
-				_name: 'CSidebarNavTitle',
-				_children: ['Penindakan']
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'SBP',
-				to: '/sbp',
-				icon: 'cil-file'
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'BA Penolakan SBP 1',
-				to: '/tolak1',
-				icon: 'cil-file'
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'BA Penolakan SBP 2',
-				to: '/tolak2',
-				icon: 'cil-file'
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'LPHP',
-				to: '/lphp',
-				icon: 'cil-file'
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'LP',
-				to: '/lp',
-				icon: 'cil-file'
+				_name: 'CSidebarNavDropdown',
+				name: 'PENINDAKAN',
+				icon: 'cil-clone',
+				_children: [
+					{
+						_name: 'CSidebarNavDropdown',
+						name: 'SBP',
+						icon: 'cil-list',
+						_children: [
+							{
+								_name: 'CSidebarNavItem',
+								name: 'Input SBP',
+								to: '/sbp',
+								icon: 'cil-file'
+							},
+							{
+								_name: 'CSidebarNavItem',
+								name: 'BA Penolakan SBP 1',
+								to: '/tolak1',
+								icon: 'cil-file'
+							},
+							{
+								_name: 'CSidebarNavItem',
+								name: 'BA Penolakan SBP 2',
+								to: '/tolak2',
+								icon: 'cil-file'
+							},
+						]
+					},
+					{
+						_name: 'CSidebarNavItem',
+						name: 'LPHP',
+						to: '/lphp',
+						icon: 'cil-file'
+					},
+					{
+						_name: 'CSidebarNavItem',
+						name: 'LP',
+						to: '/lp',
+						icon: 'cil-file'
+					},
+				]
 			},
 		]
 	}
