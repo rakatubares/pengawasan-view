38cf8315286a7190f693a4a8ebdc4cc03fcc0c0d
rakatubares <rakatubares@gmail.com>
2022-02-17 09:25:26 +0700

    reshape menu

--- src/containers/_mynav.js
+++ src/containers/_mynav.js
@@ -9,15 +9,25 @@ export default [
 				icon: 'chart'
 			},
 			{
-				_name: 'CSidebarNavTitle',
-				_children: ['Penindakan']
-			},
-			{
-				_name: 'CSidebarNavItem',
-				name: 'BA Reekspor',
-				to: '/reekspor',
-				icon: 'cil-file'
-			},
+				_name: 'CSidebarNavDropdown',
+				name: 'PENINDAKAN',
+				icon: 'cil-clone',
+				_children: [
+					{
+						_name: 'CSidebarNavDropdown',
+						name: 'Berita Acara',
+						icon: 'cil-list',
+						_children: [
+							{
+								_name: 'CSidebarNavItem',
+								name: 'BA Reekspor',
+								to: '/reekspor',
+								icon: 'cil-file'
+							},
+						]
+					}
+				]
+			}
 		]
 	}
 ]
