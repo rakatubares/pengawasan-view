c219012f13676b660a54957271ad9db6f7197383
rakatubares <rakatubares@gmail.com>
2022-07-08 07:27:59 +0700

    modify entity details in segel

--- src/views/pdf/PdfBukaSegel.js
+++ src/views/pdf/PdfBukaSegel.js
@@ -90,7 +90,7 @@ class PdfBukaSegel extends Pdf {
 		this.ln += this.props.font.height
 		this.pdf.text('Alamat', this.props.ind.alp, this.ln)
 		this.pdf.text(':', this.props.ind.cln2, this.ln)
-		this.pdf.text(converters.string(this.data.dokumen.bukasegel.saksi.alamat).replace('\n', ' '), this.props.ind.txt2, this.ln)
+		this.pdf.text(converters.string(this.data.dokumen.bukasegel.saksi.alamat_identitas).replace('\n', ' '), this.props.ind.txt2, this.ln)
 		this.ln += this.props.font.height
 		this.pdf.text('Pekerjaan', this.props.ind.alp, this.ln)
 		this.pdf.text(':', this.props.ind.cln2, this.ln)
